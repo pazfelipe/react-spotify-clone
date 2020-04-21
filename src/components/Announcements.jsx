25cc6fc7bf437fcb7247f85410e365c083299339
Felipe Paz <xxfelipepaz@gmail.com>
2020-04-21 10:07:58 -0300

    Minor fixes on banner announcement

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
-export default function Annoucements ( { thumbnail, title, description } ) {
+export default function Announcements ( { thumbnail, title, description } ) {
 
   const [isHiding, setIsHiding] = useState( false )
   const [top, setTop] = useState( -3000 )
   const [left, setLeft] = useState( -3000 )
+  const [following, setFollow] = useState( false )
+  const [labelFollow, setLabelFollow] = useState( 'follow' )
 
   const _hide = () => {
     setIsHiding( true )
@@ -20,6 +22,19 @@ export default function Annoucements ( { thumbnail, title, description } ) {
     return top
   }
 
+  const _setLabelFollowOnOver = () => {
+    if ( following ) {
+      return setLabelFollow( 'unfollow' )
+    }
+    setLabelFollow( 'follow' )
+  }
+  const _setLabelFollowOnLeave = () => {
+    if ( following ) {
+      setLabelFollow( 'unfollow' )
+    }
+    setLabelFollow( 'follow' )
+  }
+
   useEffect( () => {
     document.addEventListener( 'click', function ( event ) {
       const elClicked = event.target
@@ -33,12 +48,12 @@ export default function Annoucements ( { thumbnail, title, description } ) {
 
   const content = (
     <React.Fragment>
-      <div className="annoucements">
-        <div className="annoucements-container">
-          <div className="annoucements-container--close" onClick={_hide} >Hide annoucements</div>
-          <div className="annoucements-background" style={{ backgroundImage: `url(${thumbnail})` }}></div>
-          <div className="annoucements-container--thumbnail" style={{ backgroundImage: `url(${thumbnail})` }}></div>
-          <div className="annoucements-container--info">
+      <div className="announcements">
+        <div className="announcements-container">
+          <div className="announcements-container--close" onClick={_hide} >Hide announcements</div>
+          <div className="announcements-background" style={{ backgroundImage: `url(${thumbnail})` }}></div>
+          <div className="announcements-container--thumbnail" style={{ backgroundImage: `url(${thumbnail})` }}></div>
+          <div className="announcements-container--info">
             <div className="title">
               <p>Playlist</p>
               <h1>{title}</h1>
@@ -46,7 +61,19 @@ export default function Annoucements ( { thumbnail, title, description } ) {
             </div>
             <div className="actions">
               <button className="actions--button play">Play</button>
-              <button className="actions--button follow">Follow</button>
+              <button
+                onMouseOver={_setLabelFollowOnOver}
+                onMouseLeave={_setLabelFollowOnLeave}
+                onClick={() => setFollow( !following )}
+                className={following
+                  ? 'actions--button follow text-green'
+                  : 'actions--button follow'}>
+                {labelFollow}
+                {
+                  following && labelFollow === 'unfollow' ? ''
+                    : following && labelFollow !== 'unfollow' ? 'ing  ' : ''
+                }
+              </button>
               <div data-menu className="actions--button menu" id="annoucementsMenu" onClick={_setMenuPosition}>
                 <span data-menu>...</span>
               </div>
@@ -57,7 +84,7 @@ export default function Annoucements ( { thumbnail, title, description } ) {
       <div
         data-menu
         id="annoucementsMenuContainer"
-        className="annoucements--menu"
+        className="announcements--menu"
         style={{ top: `${top}px`, left: `${left}px` }}>
         <ul data-menu>
           <li data-menu>Go to Playlist Radio</li>
@@ -72,31 +99,31 @@ export default function Annoucements ( { thumbnail, title, description } ) {
             <i data-menu className="las la-angle-right"></i>
             <div
               data-menu
-              className="annoucements--menu"
+              className="announcements--menu"
               style={{ top: `${_top + 140}px`, left: `${left + 400}px` }}>
               <ul data-menu>
                 <li data-menu>
-                  <i data-menu className="lab la-facebook" style={{ color: '#3b5999' }}></i>
+                  <i data-menu className="lab la-facebook-f" style={{ backgroundColor: '#3b5999' }}></i>
                   <span data-menu>Facebook</span>
                 </li>
                 <li data-menu>
-                  <i className="lab la-facebook-messenger" style={{ color: '#0084ff' }}></i>
+                  <i className="lab la-facebook-messenger" style={{ backgroundColor: '#0084ff' }}></i>
                   <span data-menu>Messenger</span>
                 </li>
                 <li data-menu>
-                  <i className="lab la-twitter" style={{ color: '#55acee' }}></i>
+                  <i className="lab la-twitter" style={{ backgroundColor: '#55acee' }}></i>
                   <span data-menu>Twitter</span>
                 </li>
                 <li data-menu>
-                  <i className="lab la-telegram" style={{ color: '#0088cc' }}></i>
+                  <i className="lab la-telegram" style={{ backgroundColor: '#0088cc' }}></i>
                   <span data-menu>Telegram</span>
                 </li>
                 <li data-menu>
-                  <i className="lab la-skype" style={{ color: '#00AFF0' }}></i>
+                  <i className="lab la-skype" style={{ backgroundColor: '#00AFF0' }}></i>
                   <span data-menu>Skype</span>
                 </li>
                 <li data-menu>
-                  <i className="lab la-tumblr" style={{ color: '#34465d' }}></i>
+                  <i className="lab la-tumblr" style={{ backgroundColor: '#34465d' }}></i>
                   <span data-menu>Tumblr</span>
                 </li>
                 <li data-menu>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import MainBase from '../layouts/MainBase'
 
-import Annoucement from '../components/Annoucements'
+import Announcement from '../components/Announcements'
 
 export default class App extends Component {
   render () {
     return (
       <MainBase>
-        <Annoucement
+        <Announcement
           title="Só tem hit! | Dua Lipa | Future Nostalgia"
           description="É hit que você quer? Nós temos! Dua Lipa, Twenty One Pilots e muito mais."
           thumbnail="/images/albums/dualipa.jpg"
