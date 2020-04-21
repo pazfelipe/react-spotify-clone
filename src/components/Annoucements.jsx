446239617fa4b5715981f0bb8f0b4e43e89a392a
Felipe Paz <xxfelipepaz@gmail.com>
2020-04-21 09:19:24 -0300

    Created menu banner announcement

--- src/components/Annoucements.jsx
+++ src/components/Annoucements.jsx
@@ -16,6 +16,10 @@ export default function Annoucements ( { thumbnail, title, description } ) {
     setLeft( el.left - 170 )
   }
 
+  const _top = () => {
+    return top
+  }
+
   useEffect( () => {
     document.addEventListener( 'click', function ( event ) {
       const elClicked = event.target
@@ -56,8 +60,58 @@ export default function Annoucements ( { thumbnail, title, description } ) {
         className="annoucements--menu"
         style={{ top: `${top}px`, left: `${left}px` }}>
         <ul data-menu>
-          <li data-menu>Go to playlist radio</li>
-          <li data-menu>Go to playlist radio</li>
+          <li data-menu>Go to Playlist Radio</li>
+          <li data-menu className="separator"></li>
+          <li data-menu>Report</li>
+          <li data-menu>Save to Your Library</li>
+          <li data-menu className="separator"></li>
+          <li data-menu className="disabled">Create similar Playlist</li>
+          <li data-menu className="separator"></li>
+          <li data-menu className="submenu">
+            <span data-menu>Share</span>
+            <i data-menu className="las la-angle-right"></i>
+            <div
+              data-menu
+              className="annoucements--menu"
+              style={{ top: `${_top + 140}px`, left: `${left + 400}px` }}>
+              <ul data-menu>
+                <li data-menu>
+                  <i data-menu className="lab la-facebook" style={{ color: '#3b5999' }}></i>
+                  <span data-menu>Facebook</span>
+                </li>
+                <li data-menu>
+                  <i className="lab la-facebook-messenger" style={{ color: '#0084ff' }}></i>
+                  <span data-menu>Messenger</span>
+                </li>
+                <li data-menu>
+                  <i className="lab la-twitter" style={{ color: '#55acee' }}></i>
+                  <span data-menu>Twitter</span>
+                </li>
+                <li data-menu>
+                  <i className="lab la-telegram" style={{ color: '#0088cc' }}></i>
+                  <span data-menu>Telegram</span>
+                </li>
+                <li data-menu>
+                  <i className="lab la-skype" style={{ color: '#00AFF0' }}></i>
+                  <span data-menu>Skype</span>
+                </li>
+                <li data-menu>
+                  <i className="lab la-tumblr" style={{ color: '#34465d' }}></i>
+                  <span data-menu>Tumblr</span>
+                </li>
+                <li data-menu>
+                  <i className="las la-link"></i>
+                  <span data-menu>Copy Playlist Link</span>
+                </li>
+                <li data-menu style={{ padding: '10px 30px', paddingLeft: '60px' }}>
+                  <span data-menu>Copy Embeded Code</span>
+                </li>
+                <li data-menu style={{ padding: '10px 30px', paddingLeft: '60px' }}>
+                  <span data-menu>Copy Spotify URI</span>
+                </li>
+              </ul>
+            </div>
+          </li>
         </ul>
       </div>
     </React.Fragment>
